a07696c17ead53d4f06e9bdf1dd4f5362c96d222
Beka Dessalegn <39644079+bekaDessalegn@users.noreply.github.com>
2023-03-18 22:51:22 +0300

    made some UI changes

--- components/add-testimonial-link.js
+++ components/add-testimonial-link.js
@@ -2,12 +2,15 @@ import React from 'react'
 import Textform from './textformfield'
 import Button2 from './button2'
 import YoutubeLinks from './youtube_links'
+import Heading from './heading'
 
 const AddTestimonialLink = () => {
   return (
     <div className='w-screen flex justify-center'>
         <div className='w-3/5 items-center border-x-2 border-textFormBorderbg px-10'>
-        <p className='text-xl font-bold mb-3 mt-10'>Youtube Links</p>
+        <div className='grid text-center gap-2 mt-16 mb-5'>
+            <Heading heading='Youtube Links' />
+      </div>
         <YoutubeLinks />
         <p className='text-xl font-bold mt-5 mb-2'>Add Testimonial Youtube Link</p>
         <Textform label="Testimonial Youtube Link" />
--- components/faqItem.js
+++ components/faqItem.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import { useState } from 'react'
-import {AiOutlineDown, AiOutlineUp} from 'react-icons/ai'
+import {AiOutlineDown, AiOutlineUp, AiOutlineEdit, AiOutlineDelete} from 'react-icons/ai'
+import Modal from './modal';
 
 const FaqItem = ({items}) => {
     const [activeIndex, setActiveIndex] = useState(0)
+    const [isOpen, setIsOpen] = useState(false);
+
+  function handleOpenModal() {
+    setIsOpen(true);
+  }
+
+  function handleCloseModal() {
+    setIsOpen(false);
+  }
 
   const handleClick = (index) => {
     setActiveIndex(index === activeIndex ? -1 : index)
   }
   return (
     <div className="accordion w-screen flex flex-col justify-center items-center">
+        <Modal isOpen={isOpen} onClose={handleCloseModal} title="Delete link">
+        <p>Are you sure you want to delete this FAQ ?</p>
+      </Modal>
       {items.map((item, index) => (
         <div className={`accordion-item ${index === activeIndex ? 'active' : ''} w-3/5 bg-surface`} key={index}>
           <div className="accordion-item-title" onClick={() => handleClick(index)}>
-            <div className=' font-semibold'>{item.question}</div>
-            <div>{index === activeIndex ? <AiOutlineUp className='text-3xl text-accentColor' /> : <AiOutlineDown className='text-3xl text-accentColor' />}</div>
+            <div className='font-semibold'>{item.question}</div>
+            <div className='flex flex-row'>
+                <div className='flex flex-row mr-4'>
+                    <AiOutlineEdit className='w-[25px] h-[25px] mr-2 cursor-pointer' />
+                    <AiOutlineDelete onClick={handleOpenModal} className='w-[25px] h-[25px] fill-dangerColor cursor-pointer' />
+                </div>
+                <div>{index === activeIndex ? <AiOutlineUp className='w-[25px] h-[25px] text-accentColor' /> : <AiOutlineDown className='w-[25px] h-[25px] text-accentColor' />}</div>
+            </div>
           </div>
           <div className="accordion-item-content">{item.answer}</div>
         </div>
