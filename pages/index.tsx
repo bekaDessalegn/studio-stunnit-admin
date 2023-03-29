75747eb27a151647d906fc4efc3feb768cc0d8a0
Estifanos Neway <Estifanos.Neway.D@gmail.com>
2023-03-29 12:16:11 +0300

    implemented Auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,41 +3,33 @@ import Projects from './projects/index'
 import apiUrl from '../config'
 
 const Home: NextPage = (projects) => {
+
   return (
     <>
-    <Projects projects={projects}/>
+      <Projects projects={projects} />
     </>
   )
 }
 
-export default Home
-
 export async function getStaticProps() {
-
   try {
     let res = await fetch(`${apiUrl}/projects`);
     let projects = await res.json();
     return {
-        props : {
-          projects: projects,
-        }
+      props: {
+        projects: projects,
+      }
     };
   } catch (error) {
     console.error(error)
 
     return {
-      props : {
-          projects : [],
-          error : error
+      props: {
+        projects: [],
+        error: error
       }
-  };
+    };
   }
 }
 
-
-
-// function addProject(project: any) {
-//   console.log("ccccccccccccccccccccccccccccccccccccccc")
-//   console.log(projects)
-//   // projects.push(project)
-// }
+export default Home
